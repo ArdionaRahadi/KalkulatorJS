b0259491ca63526291327fb6555562c61a031f74
AlphaOmega <ardionabd08@gmail.com>
2024-09-12 20:09:49 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,25 @@
+function angka(number) {
+    document.getElementById("input").value =
+        document.getElementById("input").value + number;
+}
+
+function samaDengan() {
+    if (document.getElementById("input").value == "") {
+        document.getElementById("input").value = "Masukkan Angkanya";
+    } else {
+        let hasil = document.getElementById("input").value;
+        let test = (document.getElementById("input").value = "Hello World!!");
+    }
+}
+
+function hapus() {
+    let result = document.getElementById("input").value;
+    document.getElementById("input").value = result.substring(
+        0,
+        result.length - 1
+    );
+}
+
+function reset() {
+    document.getElementById("input").value = "";
+}
